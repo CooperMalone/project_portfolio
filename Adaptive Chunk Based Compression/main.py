69d1da0b35c16a62f9138160cd9968cdc8b4e44a
CooperMalone <124123270+CooperMalone@users.noreply.github.com>
2026-07-15 19:41:48 -0500

    Add files via upload

---
+++ Adaptive Chunk Based Compression/main.py
@@ -0,0 +1,10 @@
+from comparisons.vByte_Elias import vByteEliasComparison
+from comparisons.vByte_bitPack import vByteBitPackComparison
+from comparisons.synthetic import run_all_tests
+from comparisons.adaptive_comp import adaptiveComparison
+
+if __name__ == "__main__":
+    # vByteEliasComparison()
+    # vByteBitPackComparison()
+    run_all_tests()
+    #adaptiveComparison()
